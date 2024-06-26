3a89f596e66150e526474b3f4886c367c60c6018
d-berbattov <berbattov.tech@gmail.com>
2024-06-26 19:07:06 -0300

    📝 Update files - getting data from input form

---
+++ ex004/cad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Resultado</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Resultado do processamento</h1>
+  </header>
+  <main>
+    <?php 
+      $nome = $_GET["nome"] ?? "Nome não enviado!";
+      $sobrenome = $_GET["sobrenome"] ?? "Sobrenome não enviado!";
+
+      echo "<p>É um prazer te conhecer, <strong> $nome $sobrenome</strong></>";
+
+    ?>
+    <p><a href="javascript:history.go(-1)">Voltar</a></p>
+  </main>
+</body>
+</html>
