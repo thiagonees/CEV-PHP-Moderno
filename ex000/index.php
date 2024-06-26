6412aa24b49dd506d157b203f63cc159a74ce948
d-berbattov <berbattov.tech@gmail.com>
2024-06-26 16:17:11 -0300

    🎉 First commit

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    <?php
+    echo "Olá, mundo! \u{1F30E}"
+    ?>
+</h1>
+<p>Vamos tantar nos livrar da maldição!</p>
+</body>
+</html>
